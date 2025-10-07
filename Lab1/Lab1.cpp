35859798abf0d249f16ce552d5b437f7369334b3
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2025-10-07 17:19:50 +0200

    Add files via upload

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+std::mutex mtx;
+
+int counter;
+
+void zwiekszanie(int id) {
+    for (int i = 0; i < 10000; i++) {
+        mtx.lock();
+        ++counter;
+        cout << "Watek " << id << " zwiekszyl licznik: " << counter << std::endl;
+        mtx.unlock();
+    }
+}
+
+void zmniejszanie(int id) {
+    for (int i = 0; i < 10000; i++) {
+        mtx.lock();
+        --counter;
+        cout << "Watek " << id << " zmniejszyl licznik: " << counter << std::endl;
+        mtx.unlock();
+    }
+}
+
+int main()
+{
+    std::cout << "Uruchamianie watkow..." << std::endl;
+
+    std::thread thread1(zwiekszanie, 1);
+    std::thread thread2(zmniejszanie, 2);
+
+    thread1.join();
+    thread2.join();
+
+    std::cout << "Wszystkie watki zakonczyly prace. Koncowa wartosc licznika: " << counter << std::endl;
+
+    return 0;
+}
